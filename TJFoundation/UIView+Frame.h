7b9f1336b3cd8fa0290e20b780543a8e9ec25b9e
Thomas Joulin <toutankharton@gmail.com>
2012-12-17 12:02:40 +0700

    adding setWidth and setHeight to UIView

--- TJFoundation/UIView+Frame.h
+++ TJFoundation/UIView+Frame.h
@@ -22,6 +22,8 @@
 
 - (void)setX:(CGFloat)x;
 - (void)setY:(CGFloat)y;
+- (void)setWidth:(CGFloat)width;
+- (void)setHeight:(CGFloat)height;
 - (void)setSize:(CGSize)size;
 - (void)setOrigin:(CGPoint)origin;
 
--- TJFoundation/UIView+Frame.m
+++ TJFoundation/UIView+Frame.m
@@ -31,6 +31,24 @@
     self.frame = frame;
 }
 
+- (void)setWidth:(CGFloat)width
+{
+    CGRect frame = self.frame;
+    
+    frame.size.width = width;
+    
+    self.frame = frame;
+}
+
+- (void)setHeight:(CGFloat)height
+{
+    CGRect frame = self.frame;
+    
+    frame.size.height = height;
+    
+    self.frame = frame;
+}
+
 - (void)setSize:(CGSize)size
 {
     CGRect frame = self.frame;
